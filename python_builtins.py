984804954b7a42cbd01f6bd12f64f4039746559c
honey336 <oyinokeowo@gmail.com>
2022-02-20 18:09:09 -0500

    python builtins & keywords

---
+++ python_builtins.py
@@ -0,0 +1,33 @@
+#! /c/Users/oyino/Documents/devsearch/honey/Scripts/python
+
+'input':# We use the input() function to take input from the user
+
+name = input("Enter name: ")
+print(name)
+
+'tuple'  '''is also an ordered collection of items.tuples are immutable (elements of a tuple cannot be changed).
+To create a tuple, we need to wrap items inside parentheses () and separate items by a comma.'''
+
+numbers = (21, -5, 6, 9)
+print(numbers)
+
+'type' : This function is used to check the datatype of a particular expression or statement.
+
+number = input("Enter a number: ")
+print(type(number))
+
+Output
+
+<class 'str'>means it's a string.
+
+# print type of integers and floats
+
+number1 = 5
+print(type(number1))
+
+number2 = 5.5
+print(type(number2))
+Output
+
+<class 'int'>
+<class 'float'>
---
+++ python_keywords.py
@@ -0,0 +1,9 @@
+# False	await		else		import	
+# None	break		except		in	
+# True	class		finally		is	
+# and	continue	for			try
+# as	def			non			while
+# assert del			not			lambda
+# async	elif		if			yield
+# from	global		or			local
+# with	raise		return		pass
